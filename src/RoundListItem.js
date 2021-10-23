e1514fe4273b00d1ae2f42735e1e84c0eadc6367
tabrasel <tatebrasel@gmail.com>
2021-10-22 20:33:48 -0700

    feat: move round number tab beneath RoundIcon

--- src/RoundListItem.js
+++ src/RoundListItem.js
@@ -1,15 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './RoundListItem.css';
 
 import RoundIcon from './RoundIcon.js';
 
 function RoundListItem({number, participants, albums}) {
   return (
-    <div className="RoundListItem m-3 position-relative">
+    <div className="RoundListItem m-3">
       <RoundIcon albums={albums} />
 
-      <div className="round-number-label">
-        <p>{number}</p>
-      </div>
+      <p className="text-center">{number}</p>
     </div>
   );
 }
