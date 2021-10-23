c98eb31852f7b0324fd75f3a03b6788af56113c4
tabrasel <tatebrasel@gmail.com>
2021-10-22 18:30:34 -0700

    feat: make NavBar sticky

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav className="NavBar navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Music Club</a>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Rounds</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Members</a>
-          </li>
-        </ul>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Rounds</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Members</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
