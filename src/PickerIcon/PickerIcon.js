cb1b13e994dad2b077776868594129d3fd9f18bd
tabrasel <tatebrasel@gmail.com>
2021-10-28 23:00:45 -0700

    feat: add color to picker icons

--- src/PickerIcon/PickerIcon.js
+++ src/PickerIcon/PickerIcon.js
@@ -2,7 +2,7 @@ import './PickerIcon.css';
 
 function PickerIcon({picker}) {
   return (
-    <div className="PickerIcon rounded-circle d-flex justify-content-center align-items-center">
+    <div className="PickerIcon rounded-circle d-flex justify-content-center align-items-center" style={{backgroundColor: picker.color}}>
       <p>{picker.firstName[0] + picker.lastName[0]}</p>
     </div>
   );
