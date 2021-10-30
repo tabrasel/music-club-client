97d55c70e9670510c532fad6a92138f46e5fa482
tabrasel <tatebrasel@gmail.com>
2021-10-30 12:44:20 -0700

    feat: reorient RoundAlbumListItem on small screens

--- src/RoundAlbumListItem/RoundAlbumListItem.js
+++ src/RoundAlbumListItem/RoundAlbumListItem.js
@@ -9,21 +9,29 @@ function RoundAlbumListItem({album, participants}) {
 
   return (
     <div className="RoundAlbumListItem mb-5">
-      <div className="row">
-        <div className="col-4">
+      <div className="row gx-5 ">
+        <div className="col-sm-4">
           <div className="d-flex flex-column align-items-center">
             <img className="mb-3" src={album.imageUrl} alt={album.title + ' image'} />
             <h2 className="text-center">{album.title}</h2>
-            <h3 className="text-center">{album.artist}</h3>
+            <h3 className="mb-4 text-center">{album.artist}</h3>
           </div>
         </div>
 
-        <div className="col">
+        <div className="col-sm-8">
           <div className="list-group">
             {
               pickedTracks.map((pickedTrack) => {
                 const pickers = participants.filter((participant) => pickedTrack.pickerIds.includes(participant.id));
-                return (<PickedTrackListItem key={pickedTrack.trackNumber} pickedTrack={pickedTrack} pickers={pickers} isTopTrack={pickedTrack.trackNumber === album.topTrackNumber}/>);
+                const isTopTrack = pickedTrack.trackNumber === album.topTrackNumber;
+                return (
+                  <PickedTrackListItem
+                    key={pickedTrack.trackNumber}
+                    pickedTrack={pickedTrack}
+                    pickers={pickers}
+                    isTopTrack={isTopTrack}
+                  />
+                );
               })
             }
           </div>
