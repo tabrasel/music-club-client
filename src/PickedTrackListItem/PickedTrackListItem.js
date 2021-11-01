8acc75bc9e114d79e8c36c296060e9a71ba4cbfa
tabrasel <tatebrasel@gmail.com>
2021-10-31 18:10:29 -0700

    feat: lay out PickedTrackListItems content in columns

--- src/PickedTrackListItem/PickedTrackListItem.js
+++ src/PickedTrackListItem/PickedTrackListItem.js
@@ -1,27 +1,29 @@
 import './PickedTrackListItem.css'
 
-import { useState, useEffect } from 'react';
-
 import PickerIcon from '../PickerIcon/PickerIcon';
 
 function PickedTrackListItem({pickedTrack, pickers, isTopTrack}) {
   return (
-    <div className="PickedTrackListItem list-group-item px-2 py-1" style={{backgroundColor: isTopTrack ? '#cde3bb' : 'white'}}>
-      <div className="row">
+    <div className="PickedTrackListItem list-group-item px-3 py-2" style={{backgroundColor: isTopTrack ? '#cde3bb' : 'white'}}>
+
+      <div className="row g-0 h-100">
         <div className="col-9">
-          <div className="row">
-            <div className="col-1">
-              <p className="mx-1 my-0">{pickedTrack.trackNumber}.</p>
+          <div className="row h-100">
+            <div className="col-1 d-flex align-items-center">
+              <p className="m-0 text-right">{pickedTrack.trackNumber}.</p>
             </div>
-            <div className="col">
+            <div className="col-11 d-flex align-items-center">
               <p className="m-0">{pickedTrack.title}</p>
             </div>
           </div>
         </div>
-        <div className="col d-flex justify-content-end">
+
+        <div className="col-3 d-flex justify-content-end">
           {
             pickers.map((picker) => (
-              <PickerIcon picker={picker} />
+              <PickerIcon
+                key={picker.id}
+                picker={picker} />
             ))
           }
         </div>
