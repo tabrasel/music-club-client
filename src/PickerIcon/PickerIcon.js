e2d52fbfe1b0a2ab6a231c8f0e0cab9b4f9ad2b1
tabrasel <tatebrasel@gmail.com>
2021-11-02 16:59:27 -0700

    refactor: use css module in PickerIcon

--- src/PickerIcon/PickerIcon.js
+++ src/PickerIcon/PickerIcon.js
@@ -1,8 +1,8 @@
-import './PickerIcon.css';
+import styles from './PickerIcon.module.css';
 
 function PickerIcon({picker}) {
   return (
-    <div className="PickerIcon rounded-circle d-flex justify-content-center align-items-center" style={{backgroundColor: picker.color}}>
+    <div className={`${styles.PickerIcon} rounded-circle d-flex justify-content-center align-items-center`} style={{backgroundColor: picker.color}}>
       <p>{picker.firstName[0] + picker.lastName[0]}</p>
     </div>
   );
