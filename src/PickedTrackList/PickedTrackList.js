5fc530ae7286bcfd42e85f88c75701a99526feee
tabrasel <tatebrasel@gmail.com>
2021-11-02 16:55:58 -0700

    feat: add border to PickedTrackList

--- src/PickedTrackList/PickedTrackList.js
+++ src/PickedTrackList/PickedTrackList.js
@@ -1,15 +1,17 @@
+import styles from './PickedTrackList.module.css';
+
 import PickedTrackListItem from '../PickedTrackListItem/PickedTrackListItem';
 
 function PickedTrackList({album, participants}) {
   const pickedTracks = album.pickedTracks.sort((a, b) => a.trackNumber - b.trackNumber);
 
   return (
-    <div className="list-group">
+    <div className={`${styles.PickedTrackList} list-group`}>
       {
         pickedTracks.map((pickedTrack) => {
           const pickers = participants.filter((participant) => pickedTrack.pickerIds.includes(participant.id));
           const isTopTrack = pickedTrack.trackNumber === album.topTrackNumber;
-          
+
           return (
             <PickedTrackListItem
               key={pickedTrack.trackNumber}
