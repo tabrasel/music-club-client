8f2efccec79b1c543f02e6df6a7aa6904f17f747
tabrasel <tatebrasel@gmail.com>
2021-11-03 00:02:20 -0700

    feat: change PickedTrackList from list group to table

--- src/PickedTrackList/PickedTrackList.js
+++ src/PickedTrackList/PickedTrackList.js
@@ -6,23 +6,32 @@ function PickedTrackList({album, participants}) {
   const pickedTracks = album.pickedTracks.sort((a, b) => a.trackNumber - b.trackNumber);
 
   return (
-    <div className={`${styles.PickedTrackList} list-group`}>
-      {
-        pickedTracks.map((pickedTrack) => {
-          const pickers = participants.filter((participant) => pickedTrack.pickerIds.includes(participant.id));
-          const isTopTrack = pickedTrack.trackNumber === album.topTrackNumber;
+    <table className="table">
+      <thead className="table-dark">
+        <tr>
+          <th scope="col" style={{width: '5%'}}>#</th>
+          <th scope="col">Title</th>
+          <th scope="col" style={{width: '25%', textAlign: 'right'}}>Votes</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          pickedTracks.map((pickedTrack) => {
+            const pickers = participants.filter((participant) => pickedTrack.pickerIds.includes(participant.id));
+            const isTopTrack = pickedTrack.trackNumber === album.topTrackNumber;
 
-          return (
-            <PickedTrackListItem
-              key={pickedTrack.trackNumber}
-              pickedTrack={pickedTrack}
-              pickers={pickers}
-              isTopTrack={isTopTrack}
-            />
-          );
-        })
-      }
-    </div>
+            return (
+              <PickedTrackListItem
+                key={pickedTrack.trackNumber}
+                pickedTrack={pickedTrack}
+                pickers={pickers}
+                isTopTrack={isTopTrack}
+              />
+            );
+          })
+        }
+      </tbody>
+    </table>
   );
 }
 
--- src/PickedTrackListItem/PickedTrackListItem.js
+++ src/PickedTrackListItem/PickedTrackListItem.js
@@ -4,21 +4,11 @@ import PickerIcon from '../PickerIcon/PickerIcon';
 
 function PickedTrackListItem({pickedTrack, pickers, isTopTrack}) {
   return (
-    <div className={`${styles.PickedTrackListItem} list-group-item px-3 py-1`} style={{backgroundColor: isTopTrack ? '#cde3bb' : 'white'}}>
-
-      <div className="row g-0 h-100">
-        <div className="col-9">
-          <div className="row h-100">
-            <div className="col-1 d-flex align-items-center">
-              <p className="m-0 text-right">{pickedTrack.trackNumber}.</p>
-            </div>
-            <div className="col-11 d-flex align-items-center">
-              <p className="m-0">{pickedTrack.title}</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-3 d-flex justify-content-end">
+    <tr>
+      <th scope="row">{pickedTrack.trackNumber}</th>
+      <td className="overflow-hidden">{pickedTrack.title}</td>
+      <td>
+        <div className="d-flex justify-content-end">
           {
             pickers.map((picker) => (
               <PickerIcon
@@ -27,8 +17,8 @@ function PickedTrackListItem({pickedTrack, pickers, isTopTrack}) {
             ))
           }
         </div>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 }
 
