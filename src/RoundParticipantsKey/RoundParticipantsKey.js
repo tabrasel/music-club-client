fcc2a7575615ab3f0cd95e2ace10f70f9acd3237
tabrasel <tatebrasel@gmail.com>
2021-11-02 17:00:24 -0700

    refactor: use css module in RoundParticipantsKey

--- src/RoundParticipantsKey/RoundParticipantsKey.js
+++ src/RoundParticipantsKey/RoundParticipantsKey.js
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './RoundParticipantsKey.css';
+import styles from './RoundParticipantsKey.module.css';
 
 import PickerIcon from '../PickerIcon/PickerIcon';
 
 function RoundParticipantsKey({participants}) {
   return (
-    <div className="RoundParticipantsKey d-flex flex-column align-items-end">
+    <div className={`${styles.RoundParticipantsKey} d-flex flex-column align-items-end`}>
     {
       participants.map((participant) => (
         <div className="d-flex align-items-center mb-2" key={participant.id}>
-          <p className="fullname my-0 mx-2">{participant.firstName} {participant.lastName}</p>
+          <p className="my-0 mx-2">{participant.firstName} {participant.lastName}</p>
           <PickerIcon picker={participant} />
         </div>
       ))
