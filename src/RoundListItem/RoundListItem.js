be738f7b459fe79578b031f620be165020d7ce4f
tabrasel <tatebrasel@gmail.com>
2021-11-04 09:12:23 -0700

    refactor: use css module in RoundListItem

--- src/RoundListItem/RoundListItem.js
+++ src/RoundListItem/RoundListItem.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './RoundListItem.css';
+
+import styles from './RoundListItem.module.css';
 
 import { Link } from 'react-router-dom';
 
@@ -7,11 +8,11 @@ import RoundIcon from '../RoundIcon/RoundIcon.js';
 
 function RoundListItem({round, participants, albums}) {
   return (
-    <div className="RoundListItem position-relative m-4">
-      <div className="disk"></div>
+    <div className={`${styles.RoundListItem} position-relative m-4`}>
+      <div className={styles.disk}></div>
       <Link to={'/round/' + round.id}>
           <RoundIcon round={round} albums={albums} />
-          <div className="round-icon-overlay"></div>
+          <div className={styles.roundIconOverlay}></div>
       </Link>
     </div>
   );
