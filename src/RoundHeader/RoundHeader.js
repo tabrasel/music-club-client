37d97e110c3061efceb8c29f8d3bd7338bcf0d87
tabrasel <tatebrasel@gmail.com>
2021-11-04 09:05:39 -0700

    feat: aesthetic updates to Round components

--- src/RoundHeader/RoundHeader.js
+++ src/RoundHeader/RoundHeader.js
@@ -5,7 +5,7 @@ import RoundParticipantsKey from '../RoundParticipantsKey/RoundParticipantsKey';
 
 function RoundHeader({round, participants}) {
   return (
-    <div className={`${styles.RoundHeader} mt-3 d-flex justify-content-between align-items-center`}>
+    <div className={`${styles.RoundHeader} mt-3 d-flex justify-content-between`}>
       <div>
         <h1 className="m-0">Round {round.number}</h1>
         <h2>{round.startDate} to {round.endDate}</h2>
