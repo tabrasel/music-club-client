4dd3322e9814972e9c08ece293e936f3402aab0d
tabrasel <tatebrasel@gmail.com>
2021-12-04 22:51:45 -0800

    feat: format round time range

--- src/Round/RoundHeader/RoundHeader.js
+++ src/Round/RoundHeader/RoundHeader.js
@@ -1,14 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './RoundHeader.module.css'
 
+import { DateTime } from 'luxon';
+
 import RoundParticipantsKey from '../RoundParticipantsKey/RoundParticipantsKey';
 
 function RoundHeader({round, participants}) {
+  const startDate = DateTime.fromISO(round.startDate);
+  const endDate = DateTime.fromISO(round.endDate);
+  const dayNumber = Math.floor(endDate.diff(startDate, 'days').days);
+
+  const startDateStr = startDate.toLocaleString(DateTime.DATE_MED);
+  const endDateStr = endDate.toLocaleString(DateTime.DATE_MED);
+  const dayNumberStr = (dayNumber === 1) ? '1 day' : dayNumber + ' days';
+
   return (
     <div className={`${styles.RoundHeader} mt-3 d-flex justify-content-between`}>
       <div>
         <h1 className="m-0">Round {round.number}</h1>
-        <h2 className="m-0">{round.startDate} to {round.endDate}</h2>
+        <h2 className="m-0">{startDateStr} to {endDateStr} ({dayNumberStr})</h2>
         {
           round.description ? <p className={`${styles.roundDescription} mt-4`}>{ round.description}</p> : null
         }
