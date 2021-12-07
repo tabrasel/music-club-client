058f7407210baa5e21a96f465b584480786f94be
tabrasel <tatebrasel@gmail.com>
2021-12-06 22:45:23 -0800

    refactor: replace bootstrap classes with module classes

--- src/ClubView/ParticipantStatusIcon/ParticipantStatusIcon.js
+++ src/ClubView/ParticipantStatusIcon/ParticipantStatusIcon.js
@@ -9,7 +9,7 @@ function ParticipantStatusIcon({participant}) {
     <div className={styles.ParticipantStatusIcon}>
       <div className={styles.spinner} style={{borderTopColor: participant.color, borderRightColor: participant.color, borderBottomColor: participant.color}}></div>
 
-      <div className={`${styles.icon} rounded-circle d-flex justify-content-center align-items-center`} style={{backgroundColor: participant.color}}>
+      <div className={styles.icon} style={{backgroundColor: participant.color}}>
         <p>{participant.firstName[0] + participant.lastName[0]}</p>
       </div>
     </div>
