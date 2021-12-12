4f4d3c263f864c6d7d8f3d1fd8207139c367bfbc
tabrasel <tatebrasel@gmail.com>
2021-12-11 17:20:49 -0800

    feat: show club member icons

--- src/ClubView/ClubView.js
+++ src/ClubView/ClubView.js
@@ -1,12 +1,29 @@
 import './ClubView.css';
 
+import { useState, useEffect } from 'react';
+
 import CurrentRoundJumbotron from './CurrentRoundJumbotron/CurrentRoundJumbotron';
+import MembersList from './MembersList/MembersList';
 import PastRoundsList from './PastRoundsList/PastRoundsList';
 
 function ClubView() {
+  const [club, setClub] = useState(null);
+
+  useEffect(() => {
+    const fetchClub = async () => {
+      const res = await fetch('https://tb-music-club.herokuapp.com/api/club?id=04d9a851-61a1-476a-bc87-a3a30fc6a353');
+      const club = await res.json();
+      setClub(club);
+    };
+    fetchClub();
+  }, []);
+
   return (
     <div className="h-100">
+      <MembersList memberIds={ club ? club.participantIds : null } />
+
       <CurrentRoundJumbotron />
+
       <PastRoundsList />
     </div>
   );
---
+++ src/ClubView/MembersList/MembersList.js
@@ -0,0 +1,35 @@
+import styles from './MembersList.module.css';
+
+import { useState, useEffect } from 'react';
+
+function MembersList({memberIds}) {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      const memberPromises = memberIds.map((memberId) => {
+        return fetch('https://tb-music-club.herokuapp.com/api/member?id=' + memberId)
+          .then((res) => res.json());
+      });
+      const members = await Promise.all(memberPromises);
+      setMembers(members);
+    };
+    fetchMembers();
+  }, []);
+
+  return (
+    <div className={styles.MembersList}>
+      {
+        members.map((member) => {
+          return (
+            <div className={styles.memberIcon} key={member.id} style={{backgroundColor: member.color}}>
+              <p>{member.firstName[0] + member.lastName[0]}</p>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default MembersList;
