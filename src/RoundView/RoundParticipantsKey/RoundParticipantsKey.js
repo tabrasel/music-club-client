effac53587b51ea08f9b6babeefe0ea77836fdce
Tate Brasel <tatebrasel@gmail.com>
2022-03-21 23:17:14 -0700

    feat: replace MemberIcon with MemberIconSmall in RoundParticipantsKey

--- src/RoundView/RoundParticipantsKey/RoundParticipantsKey.js
+++ src/RoundView/RoundParticipantsKey/RoundParticipantsKey.js
@@ -1,16 +1,18 @@
+// Import stylesheets
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './RoundParticipantsKey.module.css';
 
-import MemberIcon from '../../MemberIcon/MemberIcon';
+// Import components
+import { MemberIconSmall } from '../../MemberIcons/MemberIcons';
 
 function RoundParticipantsKey({participants}) {
   return (
     <div className={`${styles.RoundParticipantsKey} d-flex flex-column align-items-end`}>
     {
       participants.map((participant) => (
-        <div className="d-flex align-items-center mb-2" key={participant.id}>
-          <p className={`${styles.participantName} my-0 mx-2`}>{participant.firstName} {participant.lastName}</p>
-          <MemberIcon member={participant} />
+        <div className={`${styles.participantListItem} mb-2`} key={participant.id}>
+          <p className={styles.participantName}>{participant.firstName} {participant.lastName}</p>
+          <MemberIconSmall member={participant} />
         </div>
       ))
     }
