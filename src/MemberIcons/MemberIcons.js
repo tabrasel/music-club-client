3570a531525c6725c71a0e8cf3e1b0d956474c16
Tate Brasel <tatebrasel@gmail.com>
2022-03-23 22:00:34 -0700

    feat: darken MemberIcons on hover

--- src/MemberIcons/MemberIcons.js
+++ src/MemberIcons/MemberIcons.js
@@ -2,7 +2,24 @@
 import styles from './MemberIcons.module.css';
 
 // Import packages
+import chroma from 'chroma-js';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// Define Link style
+const StyledLink = styled(Link)`
+  text-decoration: none;
+	background-color: ${(props) => props.defaultColor};
+  transition-duration: 0.1s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+	:hover {
+		background-color: ${(props) => props.hoverColor};
+		cursor: pointer;
+	}
+`;
 
 function MemberIconSmall({ member }) {
   return <MemberIcon member={member} sizeClass={styles.MemberIconSmall} />
@@ -17,13 +34,18 @@ function MemberIconLarge({ member }) {
 }
 
 function MemberIcon({ member, sizeClass }) {
+  const defaultColor = member.color;
+  const hoverColor = chroma(defaultColor).darken();
+
   return (
-    <Link
+    <StyledLink
       to={`/member/${member.id}`}
       className={`${styles.MemberIcon} ${sizeClass}`}
-      style={{ textDecoration: 'none', backgroundColor: member.color }}>
+      defaultColor={defaultColor}
+      hoverColor={hoverColor}
+      >
       <p>{member.firstName[0].toUpperCase() + member.lastName[0].toUpperCase()}</p>
-    </Link>
+    </StyledLink>
   );
 }
 
