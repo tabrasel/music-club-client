db67a43f7d6e9f1c68ba1ebf7168edd20156f422
Tate Brasel <tatebrasel@gmail.com>
2022-03-24 22:00:00 -0700

    feat: add loading mode to MemberIcons

--- src/MemberIcons/MemberIcons.js
+++ src/MemberIcons/MemberIcons.js
@@ -8,17 +8,20 @@ import styled from 'styled-components';
 
 // Define Link style
 const StyledLink = styled(Link)`
-  text-decoration: none;
-	background-color: ${(props) => props.defaultColor};
-  transition-duration: 0.1s;
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 50%;
+  text-decoration: none;
+	background-color: ${(props) => props.defaultColor};
+  transition-duration: 0.15s;
 	:hover {
-		background-color: ${(props) => props.hoverColor};
 		cursor: pointer;
+    transform: translateY(-2px);
 	}
+  :active {
+    background-color: ${(props) => props.hoverColor};
+  }
 `;
 
 function MemberIconSmall({ member }) {
@@ -33,20 +36,30 @@ function MemberIconLarge({ member }) {
   return <MemberIcon member={member} sizeClass={styles.MemberIconLarge} />
 }
 
-function MemberIcon({ member, sizeClass }) {
+function MemberIcon({ member, sizeClass, isLoading = false }) {
   const defaultColor = member.color;
   const hoverColor = chroma(defaultColor).darken();
 
-  return (
+  const initials = member.firstName[0].toUpperCase() + member.lastName[0].toUpperCase();
+
+  const defaultType = (
     <StyledLink
       to={`/member/${member.id}`}
       className={`${styles.MemberIcon} ${sizeClass}`}
       defaultColor={defaultColor}
-      hoverColor={hoverColor}
-      >
-      <p>{member.firstName[0].toUpperCase() + member.lastName[0].toUpperCase()}</p>
+      hoverColor={hoverColor}>
+      <p>{initials}</p>
     </StyledLink>
   );
+
+  const loadingType = (
+    <div className={`${styles.unfinishedIcon} ${sizeClass}`}>
+      <div className={styles.spinner} style={{'--c': defaultColor}}></div>
+      <p>{initials}</p>
+    </div>
+  );
+
+  return (isLoading ? loadingType : defaultType);
 }
 
 export { MemberIconSmall, MemberIconMedium, MemberIconLarge };
