0b33f2dbd1c2fe88625f7c4faa453969eeb8d6ef
Tate Brasel <tatebrasel@gmail.com>
2022-03-26 13:01:45 -0700

    feat: show unfinished participant icons in round page

--- src/RoundView/RoundAlbumListItem/RoundAlbumListItem.js
+++ src/RoundView/RoundAlbumListItem/RoundAlbumListItem.js
@@ -9,9 +9,10 @@ import { Link } from 'react-router-dom';
 
 // Import components
 import HeartbeatChart from './HeartbeatChart';
+import MissingVotesInfo from './MissingVotesInfo'
 import PickedTrackTable from '../PickedTrackTable/PickedTrackTable';
 
-function RoundAlbumListItem({album, participants, votesPerParticipant}) {
+function RoundAlbumListItem({ album, participants, votesPerParticipant }) {
   const showPickedTrackTable = album.tracks !== null && album.tracks.length > 0;
 
   const poster = participants.filter(participant => participant.id === album.posterId)[0];
@@ -20,6 +21,10 @@ function RoundAlbumListItem({album, participants, votesPerParticipant}) {
   const pickedTracksCount = album.tracks.filter((track) => track.pickerIds.length > 0).length;
   const alignmentScore = (participantsCount * votesPerParticipant - pickedTracksCount) / (participantsCount * votesPerParticipant - votesPerParticipant);
   const alignmentPercentage = Math.floor(alignmentScore * 100);
+
+  const participantVotes = createParticipantVotes(participants, album.tracks);
+  const unfinishedParticipants = participantVotes.filter((participantVotes) => participantVotes.votes.length === 0);
+  const isComplete = unfinishedParticipants.length === 0;
 
   return (
     <div className={`${styles.RoundAlbumListItem} row`}>
@@ -40,13 +45,13 @@ function RoundAlbumListItem({album, participants, votesPerParticipant}) {
         <h3 className="text-center mb-3">{album.artists.join(', ')}</h3>
 
         {
-          hasAllVotes(album.tracks, participants.length, votesPerParticipant)
+          isComplete
           ? <p className="mb-2" title="Vote overlap score. There is 100% overlap if everyone votes for the same songs, and 0% overlap if everyone votes for different songs."><FontAwesomeIcon icon={faHandshake} /> {alignmentPercentage}%</p>
-          : <p className={`${styles.missingLabel} mb-2`}>Missing votes</p>
+          : <MissingVotesInfo unfinishedParticipants={unfinishedParticipants}/>
         }
 
         {
-          hasAllVotes(album.tracks, participants.length, votesPerParticipant)
+          isComplete
           ? <div className="mb-4"><HeartbeatChart album={album} /></div>
           : null
         }
@@ -63,6 +68,31 @@ function RoundAlbumListItem({album, participants, votesPerParticipant}) {
       </div>
     </div>
   );
+}
+
+/**
+ * Creates am array of (participant, voted tracks) pairs.
+ * @param participants array of participants
+ * @param tracks       array of tracks
+ */
+function createParticipantVotes(participants, tracks) {
+  // Create a map from each participant to their voted tracks
+  const participantVotesMap = {};
+  for (let track of tracks) {
+    for (let voterId of track.pickerIds) {
+      if (!(voterId in participantVotesMap))
+        participantVotesMap[voterId] = [];
+      participantVotesMap[voterId].push(track);
+    }
+  }
+
+  // Create a list of mappings from each participant to the tracks they voted for
+  const participantVotes = participants.map((participant) => {
+    const votes = (participant.id in participantVotesMap) ? participantVotesMap[participant.id] : [];
+    return { participant, votes };
+  });
+
+  return participantVotes;
 }
 
 /**
